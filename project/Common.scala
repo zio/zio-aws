94c536467230b5a356d3f5528672fc3d6d18310b
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2020-11-17 07:08:50 +0000

    Update http4s-blaze-client, http4s-dsl to 0.21.9

--- project/Common.scala
+++ project/Common.scala
@@ -19,7 +19,7 @@ object Common extends AutoPlugin {
 
     val awsVersion = "2.15.19"
     val awsSubVersion = awsVersion.drop(awsVersion.indexOf('.') + 1)
-    val http4sVersion = "0.21.8"
+    val http4sVersion = "0.21.9"
     val fs2Version = "2.4.5"
 
     val majorVersion = "3"
