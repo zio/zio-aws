730f9559c097d8fc41e41c2153c740eb3c031095
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2021-02-18 03:23:36 +0000

    Update cats-effect to 2.3.3

--- project/Common.scala
+++ project/Common.scala
@@ -15,7 +15,7 @@ object Common extends AutoPlugin {
     val zioCatsInteropVersion = "2.3.1.0"
     val zioReactiveStreamsInteropVersion = "1.3.0.7-2"
     val zioConfigVersion = "1.0.0"
-    val catsEffectVersion = "2.3.1"
+    val catsEffectVersion = "2.3.3"
 
     val awsVersion = "2.16.0"
     val awsSubVersion = awsVersion.drop(awsVersion.indexOf('.') + 1)
