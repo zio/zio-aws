a43f88f4f5eab770f77b0f0e21f23c8d393db658
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2021-04-12 03:30:18 +0000

    Update zio, zio-streams, zio-test, ... to 1.0.6

--- project/Common.scala
+++ project/Common.scala
@@ -11,7 +11,7 @@ import scala.collection.JavaConverters._
 object Common extends AutoPlugin {
 
   object autoImport {
-    val zioVersion = "1.0.5"
+    val zioVersion = "1.0.6"
     val zioCatsInteropVersion = "2.4.0.0"
     val zioReactiveStreamsInteropVersion = "1.3.0.7-2"
     val zioConfigVersion = "1.0.4"
