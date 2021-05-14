000fb1461d42d2f32b5cf0d39b1793c46a398938
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2021-05-14 03:40:54 +0000

    Update fs2-reactive-streams to 2.5.6

--- project/Common.scala
+++ project/Common.scala
@@ -20,7 +20,7 @@ object Common extends AutoPlugin {
     val awsVersion = "2.16.62"
     val awsSubVersion = awsVersion.drop(awsVersion.indexOf('.') + 1)
     val http4sVersion = "0.21.22"
-    val fs2Version = "2.5.5"
+    val fs2Version = "2.5.6"
 
     val majorVersion = "3"
     val zioAwsVersionPrefix = s"$majorVersion.$awsSubVersion."
