82d24ea0f167cd265cb5fdb2e569379cf883c289
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2021-06-16 03:23:33 +0000

    Update aws-core, http-client-spi, ... to 2.16.85

--- project/Common.scala
+++ project/Common.scala
@@ -17,7 +17,7 @@ object Common extends AutoPlugin {
     val zioConfigVersion = "1.0.6"
     val catsEffectVersion = "2.5.1"
 
-    val awsVersion = "2.16.79"
+    val awsVersion = "2.16.85"
     val awsSubVersion = awsVersion.drop(awsVersion.indexOf('.') + 1)
     val http4sVersion = "0.21.22"
     val fs2Version = "2.5.6"
