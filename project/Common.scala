1e30672771d2c2ea6e7a97b90e524fb8eb07f769
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-01-21 03:19:28 +0000

    Update scala3-library to 3.1.1

--- project/Common.scala
+++ project/Common.scala
@@ -28,7 +28,7 @@ object Common extends AutoPlugin {
 
     val scala212Version = "2.12.15"
     val scala213Version = "2.13.8"
-    val scala3Version = "3.1.0"
+    val scala3Version = "3.1.1"
 
     val scalacOptions212 = Seq("-Ypartial-unification", "-deprecation")
     val scalacOptions213 = Seq("-deprecation")
