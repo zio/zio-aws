567ad79c66b737e84ea3d4fdfd111c165ce5a2c4
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-05-03 03:53:30 +0000

    Update zio, zio-streams, zio-test, ... to 2.0.0-RC6

--- project/Common.scala
+++ project/Common.scala
@@ -11,7 +11,7 @@ import scala.collection.JavaConverters._
 object Common extends AutoPlugin {
 
   object autoImport {
-    val zioVersion = "2.0.0-RC5"
+    val zioVersion = "2.0.0-RC6"
     val zioMockVersion = "1.0.0-RC5"
     val zioCatsInteropVersion = "3.3.0-RC6"
     val zioReactiveStreamsInteropVersion = "2.0.0-RC6"
