f0afb5deb1fb7f5703352ebd75c1e178f6ac2fb5
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-05-04 03:58:16 +0000

    Update zio-interop-cats to 3.3.0-RC7

--- project/Common.scala
+++ project/Common.scala
@@ -13,7 +13,7 @@ object Common extends AutoPlugin {
   object autoImport {
     val zioVersion = "2.0.0-RC5"
     val zioMockVersion = "1.0.0-RC5"
-    val zioCatsInteropVersion = "3.3.0-RC6"
+    val zioCatsInteropVersion = "3.3.0-RC7"
     val zioReactiveStreamsInteropVersion = "2.0.0-RC6"
     val zioConfigVersion = "3.0.0-RC8"
     val zioPreludeVersion = "1.0.0-RC13"
