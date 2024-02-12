029a7e66373cdf63e020c2cdc43f9a63f621321d
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-02-12 03:13:42 +0000

    Update zio-prelude to 1.0.0-RC23

--- project/Common.scala
+++ project/Common.scala
@@ -16,7 +16,7 @@ object Common extends AutoPlugin {
     val zioCatsInteropVersion = "23.1.0.0"
     val zioReactiveStreamsInteropVersion = "2.0.2"
     val zioConfigVersion = "4.0.1"
-    val zioPreludeVersion = "1.0.0-RC22"
+    val zioPreludeVersion = "1.0.0-RC23"
     val catsEffectVersion = "3.5.3"
 
     val awsVersion = "2.21.15"
