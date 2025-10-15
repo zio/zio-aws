a108390becd82aebe4197b1a25a80f3ea301a3b0
Daniel Vigovszky <vigoo@mailfence.com>
2025-10-15 21:23:19 +0000

    Update scala-library to 2.13.17

--- project/Common.scala
+++ project/Common.scala
@@ -24,7 +24,7 @@ object Common extends AutoPlugin {
     val majorVersion = "7"
     val zioAwsVersionPrefix = s"$majorVersion.$awsSubVersion."
 
-    val scala213Version = "2.13.16"
+    val scala213Version = "2.13.17"
     val scala3Version = "3.3.7"
 
     val scalacOptions212 = Seq("-Ypartial-unification", "-deprecation")
