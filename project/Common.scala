94c61403713ab9982e8847bae9c74a31b1c84cff
Daniel Vigovszky <vigoo@mailfence.com>
2025-12-03 21:24:47 +0000

    Update aws-core, http-client-spi, ... to 2.40.1

--- project/Common.scala
+++ project/Common.scala
@@ -15,7 +15,7 @@ object Common extends AutoPlugin {
     val zioPreludeVersion = "1.0.0-RC44"
     val catsEffectVersion = "3.6.3"
 
-    val awsVersion = "2.39.6"
+    val awsVersion = "2.40.1"
     val awsSubVersion = awsVersion.drop(awsVersion.indexOf('.') + 1)
     val http4sVersion = "0.23.33"
     val blazeVersion = "0.23.17"
