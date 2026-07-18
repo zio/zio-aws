74d0392644f4df8e69350bab0b5106c2565ce5db
migiside <migicide@gmail.com>
2026-07-18 10:36:50 +0900

    aws sdk 2.48.3

--- project/Common.scala
+++ project/Common.scala
@@ -15,7 +15,7 @@ object Common extends AutoPlugin {
     val zioPreludeVersion = "1.0.0-RC47"
     val catsEffectVersion = "3.7.0"
 
-    val awsVersion = "2.46.21"
+    val awsVersion = "2.48.3"
     val awsSubVersion = awsVersion.drop(awsVersion.indexOf('.') + 1)
     val http4sVersion = "0.23.36"
     val blazeVersion = "0.23.17"
--- zio-aws-codegen/build.sbt
+++ zio-aws-codegen/build.sbt
@@ -1,5 +1,5 @@
 val zioVersion = "2.1.26"
-val awsVersion = "2.46.21"
+val awsVersion = "2.48.3"
 
 sbtPlugin := true
 scalaVersion := "2.12.21"
